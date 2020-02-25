5aba7937b31092d519a6030404e67c17dc4f9482
Shiva <shivaxsin@gmail.com>
2020-02-25 10:30:59 +0800

    fix: routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/', function () {
     event(new \App\Events\MessageSent( App\Sheep::find(1), App\Message::find(1)));
     return view('welcome');
 });
+
+
+Route::get('/google/auth', 'SocialiteController@RedirectToProvider');
+Route::post('/google/auth/callback', 'SocialiteController@HandleProviderCallback');
