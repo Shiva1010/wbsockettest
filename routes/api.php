64af613bd9b07dc09ef28034c27790cc89401455
Shiva <shivaxsin@gmail.com>
2020-02-26 16:43:16 +0800

    fix: add path /google/auth/checkandroidtoken

--- routes/api.php
+++ routes/api.php
@@ -40,6 +40,7 @@ Route::get('/google/auth', 'SocialiteController@RedirectToProvider');
 Route::get('/google/auth/callback', 'SocialiteController@HandleProviderCallback');
 
 Route::post('/google/auth/checktoken', 'SocialiteController@CheckToken');
+Route::post('/google/auth/checkandroidtoken', 'SocialiteController@CheckAndroidToken');
 
 Route::get('/privacypolicy','SheepController@privacypolicy');
 
