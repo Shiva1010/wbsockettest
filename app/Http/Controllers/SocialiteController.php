2ea36cc1fc540255aff6d6149fd8a69b528f41bc
Shiva <shivaxsin@gmail.com>
2020-02-27 14:10:10 +0800

    fix: comment out 'new Google_Client'

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -74,8 +74,8 @@ class SocialiteController extends Controller
 
         $CLIENT_ID ='431122609682-9kt3dot3fjeq92rkar83mor16siod2ch.apps.googleusercontent.com';
         $id_token = $request['id_token'];
-        $client = new Google_Client(['client_id' => $CLIENT_ID]);  // Specify the CLIENT_ID of the app that accesses the backend
-        $payload = $client->verifyIdToken($id_token);
+//        $client = new Google_Client(['client_id' => $CLIENT_ID]);  // Specify the CLIENT_ID of the app that accesses the backend
+        $payload = $CLIENT_ID->verifyIdToken($id_token);
 //        if ($payload) {
 //            $userid = $payload['sub'];
 //            // If request specified a G Suite domain:
