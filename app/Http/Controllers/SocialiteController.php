49623bd8f49459968d6c933bade687877dfc7ee2
Shiva <shivaxsin@gmail.com>
2020-02-27 15:06:27 +0800

    fix: test4-TestCheckAndroidToken

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -72,7 +72,7 @@ class SocialiteController extends Controller
 
     public function TestCheckAndroidToken(Request $request)
     {
-        require_once 'vendor/autoload.php';
+//        require_once 'vendor/autoload.php';
 
         // Get $id_token via HTTPS POST.
 
